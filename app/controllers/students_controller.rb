6c727a6149a116f9652abc81fae2b9c0dc912e89
Ai Mulyana <ai.mulyana@outlook.com>
2024-02-07 09:27:39 +0700

    Implement Student Create Action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+
   def index
     @students = Student.all
   end
@@ -7,6 +8,20 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :email)
   end
 end
