f872dba7151f23a885e6f4b6e26e7b0f1065fb43
Ai Mulyana <ai.mulyana@outlook.com>
2024-02-09 08:09:48 +0700

    Implementing ActiveRecord Callback in Model

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,15 @@ class Student < ApplicationRecord
   has_many :student_projects
   has_many :projects, through: :student_projects
   has_one :profile
+
+  after_create :age_calculation
+
+  def age_calculation
+    if self.date_of_birth.present?
+      age = Date.today.year - self.date_of_birth.year
+      puts "++++ Your age is #{age} ++++"
+    else
+      puts "+++ Cannot calculate age ++++"
+    end
+  end
 end
